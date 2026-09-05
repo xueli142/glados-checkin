000978608f3d7aa863a89a2737330de224daa590
xueli <1426961520@qq.com>
2026-09-05 15:02:33 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+
+
+const glados = async ()=>{
+
+    //用于存储签到结果
+    const notice = []
+    //硬编码环境变量cookie
+    if(!process.env.GLADOS){
+        notice.push("缺少cookie")
+        return notice
+    }
+    for(const cookie  of String(process.env.GLADOS).split('\n')){
+        if(!cookie)continue
+
+try{
+    const common = {
+  'cookie': cookie,
+  'referer': 'https://glados.cloud/console/checkin',
+  'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+}
+
+const action = await fetch ('https://glados.cloud/api/user/checkin',{
+    method:'POST',
+    headers:{...common,'content-type':'application/json'},
+    
+
+    body:'{"token":"glados.cloud"}'
+    }).then((r)=>r.json())
+    //如果action.code存在为真，视为签到失败
+    if(action?.code)throw new Error(action?.message)
+
+    const status = await fetch ('https://glados.cloud/api/user/status',{
+    method:'GET',
+    headers:{...common}
+//末尾加这个是解析json的操作
+    }).then((r)=>r.json())
+    if(status?.code)throw new Error(status?.message)
+            notice.push('glados签到成功',
+                `${action?.message}`,
+                `还剩下${Number(status?.data?.leftDays)}`
+)
+
+}catch(error){
+    notice.push('出错了'
+        `${error}`
+    )
+    
+}
+}
+    return notice
+}
+
+const main = async ()=>{
+const result = await glados()
+//打印签到结果
+console.log('=========签到结果==========')
+for( const line of result){
+    console.log(line)
+}
+console.log('=======================')
+
+}
+
+main()
